74e422f422ab520a529952163868630827971c00
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2018-12-31 21:46:05 +0500

    start of project

--- main.asm
+++ main.asm
@@ -1,13 +1,31 @@
 INCLUDE irvine32.inc
+INCLUDE module.inc
 
 .data
+	hrstr BYTE "Enter hour: ", 0
+	minstr BYTE "Enter minute: ", 0
+	
+	
+	hr BYTE ?
+	mnt BYTE ?
 
+	laststr BYTE "Alarm set to ", 0
+	
 .code
-    main proc
+	main proc
+	mov  eax, green
+	xor ebx, ebx
+	
+	call SetTextColor
 
-
-    exit
-    main endp
+	GETDATA hrstr, hr, bh
+	
+	GETDATA minstr, mnt, bh	
+		
+	GETDATA laststr, hr, mnt
+	
+	exit
+    	main endp
 
 
 end main
