a9fd39d552a0b9d029d158a9bdb6f5127b894dd9
ameerhamza90 <46285538+ameerhamza90@users.noreply.github.com>
2019-01-01 16:35:38 +0500

    update

--- main.asm
+++ main.asm
@@ -1,7 +1,20 @@
 INCLUDE Irvine32.inc
 INCLUDE Inputmod.inc
 
+.data
+	hrstr BYTE "Enter hour: ", 0
+	minstr BYTE "Enter minute: ", 0	
 	
+	hr BYTE ?
+	mnt BYTE ?
+
+	laststr BYTE "Your alarm is set to ", 0
+        
+        arr BYTE 500 DUP(0)
+	filename BYTE "data.txt",0
+	Bytes_Written DWORD ?
+        handle DWORD ?
+        
 .code
 	main proc
 	xor eax, eax
@@ -9,7 +22,12 @@ INCLUDE Inputmod.inc
 	xor ebx, ebx
 	
 	call SetTextColor
-
+        
+        FILEHANDLING
+        
+        mov eax, 5000
+        call delay
+        
 	GETDATA hrstr, hr, bh
 	
 	GETDATA minstr, mnt, bh	
